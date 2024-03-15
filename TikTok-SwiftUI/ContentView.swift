a0ba5002e652ffbc8b37391542017a0a0890ab40
Your Name <you@example.com>
2024-03-15 16:33:18 -0400

    Initial Commit

---
+++ TikTok-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TikTok-SwiftUI
+//
+//  Created by Chima onyekwere on 3/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TikTok-SwiftUI/TikTok_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTok_SwiftUIApp.swift
+//  TikTok-SwiftUI
+//
+//  Created by Chima onyekwere on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct TikTok_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
